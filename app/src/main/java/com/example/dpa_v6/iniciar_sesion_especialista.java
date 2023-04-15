e489fa7d34576b0e3d1147b8c0a52e5866001023
IvanV <20206262@uatx.com>
2023-04-14 22:00:59 -0600

    Initial commit

---
+++ app/src/main/java/com/example/dpa_v6/iniciar_sesion_especialista.java
@@ -0,0 +1,14 @@
+package com.example.dpa_v6;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class iniciar_sesion_especialista extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_iniciar_sesion_especialista);
+    }
+}
