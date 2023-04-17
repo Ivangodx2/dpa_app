079e0bda4b8c6c41df65780f8235af3cff4834c5
IvanV <20206262@uatx.com>
2023-04-17 01:00:00 -0600

    nueva clase home_paciente

---
+++ app/src/main/java/com/example/dpa_v6/home_paciente.java
@@ -0,0 +1,13 @@
+package com.example.dpa_v6;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+public class home_paciente extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_home_paciente);
+    }
+}
